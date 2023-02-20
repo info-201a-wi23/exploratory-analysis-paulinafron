9c9d7282021e4772710610622b004b0f27f07d8d
RIBBLY <rbaerg@uw.edu>
2023-02-20 15:27:02 -0800

    worked on formatting in index.rmd file so now my table and summary show up in R Studio

--- summary.R
+++ summary.R
@@ -1,24 +1,28 @@
+# load libraries
+library(dplyr)
+library(knitr)
+
 pet <- read.csv("Seattle_Pet_Licenses.csv")
 zipcode <- read.csv("usa_zipcode_of_wa-1564j.csv")
 
-zipcode <- zipcode %>% filter(!is.na(`Zipcode name`) & City == 'SEATTLE')
-zipcode$'ZIP Code' <- as.numeric(as.character(zipcode$'ZIP Code'))
+zipcode <- zipcode %>% filter(!is.na(`Zipcode.name`) & City == 'SEATTLE')
+zipcode$'ZIP.Code' <- as.numeric(as.character(zipcode$'ZIP.Code'))
 
 pet_summary <- list(
   nrow = nrow(pet),
   n_distinct_species = n_distinct(pet$Species),
-  n_distinct_p_breeds = n_distinct(pet$'Primary Breed'),
-  max_license_issue_date = max(pet$'License Issue Date'),
-  min_license_issue_date = min(pet$'License Issue Date')
+  n_distinct_p_breeds = n_distinct(pet$'Primary.Breed'),
+  max_license_issue_date = max(pet$'License.Issue.Date'),
+  min_license_issue_date = min(pet$'License.Issue.Date')
 )
 
 # calculate summary statistics for the zipcode dataframe
 zipcode_summary <- list(
   nrow = nrow(zipcode),
   n_distinct_cities = n_distinct(zipcode$City),
-  n_zipcodes = n_distinct(zipcode$'ZIP Code'),
-  max_zip_code = max(zipcode$'ZIP Code'),
-  min_zip_code = min(zipcode$'ZIP Code')
+  n_zipcodes = n_distinct(zipcode$'ZIP.Code'),
+  max_zip_code = max(zipcode$'ZIP.Code'),
+  min_zip_code = min(zipcode$'ZIP.Code')
 )
 
 # print the summary statistics
--- tables.R
+++ tables.R
@@ -1,27 +1,23 @@
 #load-packages
 library(tidyverse)
-library(openintro)
-library(GGally)
-library(ggridges)
-library(janitor)
-library(tidymodels)
-library(infer)
 library(broom)
 library(stringr)
 library(dplyr)
+library(knitr)
 
 #Load csv
 pet <- read.csv("Seattle_Pet_Licenses.csv")
 zipcode <- read.csv("usa_zipcode_of_wa-1564j.csv")
 
-zipcode <- zipcode %>% filter(!is.na(`Zipcode name`) & City == 'SEATTLE')
-zipcode$'ZIP Code' <- as.numeric(as.character(zipcode$'ZIP Code'))
-joined_df <- left_join(pet, zipcode, by = "ZIP Code")
+zipcode <- zipcode %>% filter(!is.na(`Zipcode.name`) & City == 'SEATTLE')
+zipcode$'ZIP.Code' <- as.numeric(as.character(zipcode$'ZIP.Code'))
+joined_df <- left_join(pet, zipcode, by = "ZIP.Code")
 
 # create the aggregate table
 agg_table <- joined_df %>%
-  select('License Issue Date', Species, 'Primary Breed', 'Secondary Breed', 'ZIP Code', 'Zipcode name', 'County Name') %>%
-  group_by('ZIP Code')
+  select('License.Issue.Date', Species, 'Primary.Breed', 'Secondary.Breed', 'ZIP.Code', 'Zipcode.name', 'County.Name') %>%
+  group_by('ZIP.Code')
 
 # display the first five tuples
-head(agg_table, 5)
+summarized_table <- head(agg_table, 5)
+summarized_table <- summarized_table[ , 1:(ncol(summarized_table)-1)]
